d2280d24278b018cf702db590d10ea58e3cbf623
Michael Pace <michael.pace@flowhub.com>
2022-01-09 16:34:47 -0700

    Request users location in AppDelegate
    - view model is set as location manager delegate
    - only monitor significant location changes
    - update plist

--- Public-WeedmapsChallenge/WeedmapsChallenge/Sources/Bootstrap/AppDelegate.swift
+++ Public-WeedmapsChallenge/WeedmapsChallenge/Sources/Bootstrap/AppDelegate.swift
@@ -3,13 +3,28 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    private lazy var locationManager: CLLocationManager = {
+        let locationManger = CLLocationManager()
+        locationManger.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locationManger.pausesLocationUpdatesAutomatically = true
+        locationManger.activityType = .otherNavigation
+        return locationManger
+    }()
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let yellowPagesAPI = YelpAPI(urlSession: URLSession.shared, decoder: JSONDecoder())
+        let searchViewModel = SearchViewModel(api: yellowPagesAPI)
+        locationManager.delegate = searchViewModel
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startMonitoringSignificantLocationChanges() // need to eventually stop tracking this
+
         let homeViewController = HomeViewController()
         let navigationController = UINavigationController(rootViewController: homeViewController)
         let window = UIWindow()
