6313eee79ab4c22de49e33558d337c41146b9746
Michael Pace <michael.pace@flowhub.com>
2022-01-09 11:25:08 -0700

    Setup shared schemes

--- Public-WeedmapsChallenge/WeedmapsChallengeTests/YelpAPIClientTests.swift
+++ Public-WeedmapsChallenge/WeedmapsChallengeTests/YelpAPIClientTests.swift
@@ -7,18 +7,7 @@ import XCTest
 
 
 class YelpAPIClientTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testBusinessModelDecodedFromJsonYieldsExpectedResult() {
-    }
-    
-    func testYelpAPIRequestYieldsExpectedResult() {
+        XCTAssertTrue(false)
     }
 }
