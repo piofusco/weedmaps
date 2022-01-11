dd0d5d7ea01220d8de6bdfdd48ac0bef27f05099
Michael Pace <michael.pace@flowhub.com>
2022-01-11 15:14:05 -0700

    Cleaned initialization of SearchCache in AppDelegate
    - no longer needs encoder/decoders

--- Public-WeedmapsChallenge/WeedmapsChallenge/Sources/Bootstrap/AppDelegate.swift
+++ Public-WeedmapsChallenge/WeedmapsChallenge/Sources/Bootstrap/AppDelegate.swift
@@ -21,9 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let decoder = JSONDecoder()
-        let encoder = JSONEncoder()
         let fileManager = FileManager.default
-        let searchCache = WeedmapsSearchCache(fileManager: fileManager, decoder: decoder, encoder: encoder)
+        let searchCache = WeedmapsSearchCache(fileManager: fileManager)
         let yellowPagesAPI = YelpAPI(urlSession: URLSession.shared, decoder: decoder)
         let searchViewModel = SearchViewModel(api: yellowPagesAPI, searchCache: searchCache)
         locationManager.delegate = searchViewModel
